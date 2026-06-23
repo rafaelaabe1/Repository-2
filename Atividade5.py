06922dce52f7b57c48804d772d3be1fde4c3189b
rafaelaabe1 <rafaelamouraabre@gmail.com>
2026-06-23 14:16:51 -0300

    Add files via upload

---
+++ Atividade5.py
@@ -0,0 +1,33 @@
+# QUESTÃO 05 – Caixa Eletrônico (5 pontos)
+
+# Um caixa eletrônico possui notas de:
+
+# R$ 100
+# R$ 50
+# R$ 20
+# R$ 10
+# R$ 5
+# R$ 2
+# R$ 1
+
+# Solicite ao usuário o valor do saque e informe a menor
+# quantidade possível de notas para realizar o pagamento.
+
+# Exemplo:
+
+# Digite o valor do saque: 287
+
+# Saída esperada:
+
+# 2 nota(s) de R$100
+# 1 nota(s) de R$50
+# 1 nota(s) de R$20
+# 1 nota(s) de R$10
+# 1 nota(s) de R$5
+# 1 nota(s) de R$2
+
+# Requisitos:
+# - Utilizar laços de repetição.
+# - Não utilizar listas.
+# - Exibir apenas as notas utilizadas.
+
